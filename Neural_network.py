8c261bd7e5a68cbc91f0a32520d8ba3e0151d708
ghosh0311 <tunai2000nov@gmail.com>
2021-08-09 16:00:09 +0530

    Added the gradient descent plot

--- Neural_network.py
+++ Neural_network.py
@@ -12,6 +12,7 @@ data = pd.read_csv("train.csv")
 data = np.array(data)
 m , n = data.shape
 np.random.shuffle(data)
+print("The dimension of MNIST dataset" , data.shape)
 
 #The second layer has 10 nodes
 #The output layer has 10 nodes
@@ -21,19 +22,33 @@ X_dev = data_dev[1:n]
 X_dev = X_dev/255. #normalize
 
 #Training sets
-data_train = data[1000:m].T
+data_train = data[1000:10000].T
 Y_train = data_train[0]
 X_train = data_train[1:n]
 X_train = X_train/255. #normalize
+print("The dimension of training dataset:" ,data_train.shape)
+
+data_test = data[10000:m].T
+Y_test = data_train[0]
+X_test = data_train[1:n]
+X_test = X_test/255.
+print("The dimension of test dataset:" ,data_test.shape)
 
 #Initializing parameters
 def init_params():
-    W1 = np.random.rand(10 , 784) 
-    b1 = np.random.rand(10 , 1) 
-    W2 = np.random.rand(10 , 10)
-    b2 = np.random.rand(10 , 1)
+    W1 = np.random.rand(10 , 784) - 0.5 
+    b1 = np.random.rand(10 , 1) - 0.5
+    W2 = np.random.rand(10 , 10) - 0.5
+    b2 = np.random.rand(10 , 1) - 0.5
 
     return W1 , b1 , W2 , b2
+
+#Cost function
+def costfunc(Y , h):
+    m = Y.size
+    cost = -(1/m)*(np.sum(np.multiply(np.log(h) , Y) + np.multiply((1-Y) , np.log(1-h))))
+    return cost
+
 
 #Sigmoid function
 def sigmoid(z):
@@ -41,13 +56,15 @@ def sigmoid(z):
     return g
 
 #Forward propagation
-def fwd_prop(W1 , b1 , W2 , b2 , X):
+def fwd_prop(W1 , b1 , W2 , b2 , X , Y):
     Z1 = W1.dot(X) + b1
     A1 = sigmoid(Z1)
     Z2 = W2.dot(A1) + b2
     A2 = sigmoid(Z2)
+    Ynew = transform(Y)
+    loss = costfunc(Ynew , A2)
 
-    return Z1 , A1 , Z2 , A2
+    return Z1 , A1 , Z2 , A2 , loss
 
 #Encoding of prediction vector y
 def transform(Y):
@@ -57,7 +74,7 @@ def transform(Y):
 
     return Ynew
 
-def deriv_relu(Z):
+def dsigmoid(Z):
     return Z > 0 
 
 
@@ -68,7 +85,7 @@ def back_prop(Z1 , A1 , Z2 , A2 , W2 , X , Y):
     dZ2 = A2 - Ynew
     dW2 = (1/m)*dZ2.dot(A1.T)
     db2 = (1/m)*np.sum(dZ2)
-    dZ1 = W2.T.dot(dZ2) + deriv_relu(Z1)
+    dZ1 = W2.T.dot(dZ2) + dsigmoid(Z1)
     dW1 = (1/m) * dZ1.dot(X.T)
     db1 = (1/m) * np.sum(dZ1)
 
@@ -94,28 +111,30 @@ def accuracy(prediction , Y):
 #Gradient Descent
 def gradient_descent(X , Y , iteration , alpha):
     W1 , b1 , W2 , b2 = init_params()
-    accur = np.zeros((iteration, 1),dtype=float)
-    
+    J_history = np.zeros((iteration, 1),dtype=float)
     for i in range(iteration):
-        Z1 , A1 , Z2 , A2 = fwd_prop(W1 , b1 , W2 , b2 , X)
+        Z1 , A1 , Z2 , A2 , loss = fwd_prop(W1 , b1 , W2 , b2 , X , Y)
+        print("Cost at iteration " ,i , "is:" , loss)
         dw1 , db1 , dw2 , db2 = back_prop( Z1 , A1 , Z2 , A2 , W2 , X , Y)
         W1 , W2 , b1 , b2 = update_param(W1 , b1 , W2 , b2 , dw1 , dw2 , db1 , db2 , alpha)
-        accur[i] = accuracy(get_predict(A2) , Y)
-        if i == iteration-1:
-            prediction = get_predict(A2)
-            print("Accuracy: " , accuracy(prediction , Y)*100 , "%")
+        J_history[i] = loss
 
-    return W1 , b1 , W2 , b2 , accur
+    return W1 , b1 , W2 , b2 , J_history
 
 
 #Setting alpha(learning rate) = 0.1
-iter = 500
-W1 , b1 , W2 , b2 , accur = gradient_descent(X_train , Y_train , iter , 0.1)
+iter = 100
+W1 , b1 , W2 , b2 , J = gradient_descent(X_train , Y_train , iter , 0.1)
 
-#Plot the accuracy curve
-plt.plot(accur , 'r')
-plt.xlim(10 , iter)
+#Testing the accuracy on test set 
+Z1t , A1t , Z2t , A2t , loss = fwd_prop(W1 , b1 , W2 , b2 , X_test , Y_test)
+#Printing the accuracy
+print("Accuracy" , accuracy(get_predict(A2t) , Y_test)*100 , "%")
+
+
+#Plot the loss curve
+plt.plot(J , 'r')
 plt.xlabel("Iterations")
-plt.ylabel("Accuracy")
-plt.title("Accuracy Curve")
+plt.ylabel("Cost")
+plt.title("Loss Curve")
 plt.show()
